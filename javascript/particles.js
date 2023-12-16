de7ae8c5d9b73d7be211cb4b1a47d5c82b670321
aman30865 <aman30865@gmail.com>
2023-12-16 16:25:39 +0530

    initial portfolio commit

---
+++ javascript/particles.js
@@ -0,0 +1,98 @@
+/* ---- particles.js config ---- */
+
+particlesJS("particles-id", {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area":1000
+        }
+      },
+      "color": {
+        "value": ["#5b86e5", "#36d1dc", "#36d1dc", "#5b86e5"]
+      },
+      
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 1,
+          "color": "#58e"
+        },
+        "polygon": {
+          "nb_sides": 2
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.5,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 150,
+          "size_min": 1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 120,
+        "color": "#000",
+        "opacity": .7,
+        "width": 1
+      },
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
